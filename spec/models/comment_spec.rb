8a30c4289f5a0b322137a60272f227c3b1d6cb74
raphael allard <raphael.s.allard@gmail.com>
2021-07-14 15:06:39 +0200

    added more tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  context '#validations' do
+    it 'should validate content length to be min 10' do
+      comment = Comment.new(content: 'Apple')
+      comment.valid?
+      expect(comment.errors.messages).to include(:content)
+      expect(comment.valid?).to be false
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Post, type: :model do
   context '#Comment association' do
     it 'should increment comments_count' do
       post = Post.new(name: "Apple vs Android")
-      post.comments.new(content: "Go Apple!")
+      post.comments.new(content: "Go Apple!jijij")
       post.save
       expect(Comment.count).to eq(1)
       expect(post.comments_count).to eq(1)
