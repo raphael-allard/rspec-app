dbabf57e5af25520677372d32cb4cbafe56ee75d
raphael allard <raphael.s.allard@gmail.com>
2021-07-14 10:59:51 +0200

    wrote first tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#Comment association' do
+    it 'should increment comments_count' do
+      post = Post.new(name: "Apple vs Android")
+      post.comments.new(content: "Go Apple!")
+      post.save
+      expect(Comment.count).to eq(1)
+      expect(post.comments_count).to eq(1)
+    end
+  end
+
+  it 'should persist a post' do
+    Post.create(name: "Apple vs Android")
+    expect(Post.count).to eq(1)
+  end
 end
